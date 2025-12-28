d5e52023e384fbb7e94612f515d87e77756406ba
indracb-dev19 <indra.cb.dev19@gmail.com>
2025-12-29 04:09:36 +0700

    implement protected routes for security

--- proxy.ts
+++ proxy.ts
@@ -1,12 +1,32 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+// implement protected route with clerk
+
+// we can use 2 approach, one is list all protected routes
+// const protectedRoutes = createRouteMatcher(["/user-profile"])
+
+// or we can list all public routes
+const publicRoutes = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"])
+
+export default clerkMiddleware(async (auth, req) => {
+    // if (protectedRoutes(req)) await auth.protect()
+
+    // also we can only use this code to protect route
+    // if (!publicRoutes(req)) await auth.protect()
+
+    // if we want more advance protection
+    const { userId, redirectToSignIn } = await auth();
+    if (!userId && !publicRoutes(req)) {
+        // do some logic here
+        return redirectToSignIn();
+    }
+});
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    String.raw`/((?!_next|[^?]*\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)`,
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+    matcher: [
+        // Skip Next.js internals and all static files, unless found in search params
+        `/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)`,
+        // Always run for API routes
+        '/(api|trpc)(.*)',
+    ],
 };
